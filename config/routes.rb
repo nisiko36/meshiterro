0eb490d1ff75508e12e80a9f11ba3eee039a4d85
KokiNishimura <89366027+nisiko36@users.noreply.github.com>
2023-09-01 08:47:45 +0000

    [finish]5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/about' => 'homes#about', as: 'about'
+  root to: 'homes#top'
   devise_for :users
-  root to: "homes#top"
+  
+  resources :post_images, only: [:new, :index, :show]
+  get "/homes/about" => "homes#about", as: "about"
 end
